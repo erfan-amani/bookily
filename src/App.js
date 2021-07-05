81da00cbcee1e935b02c3836b3c7d4dc1f46c87d
erfan-amani <erfan80amani23@gmail.com>
2021-07-05 21:09:06 +0430

    initialized, styled and imported new book

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import BooksList from './Books/‌BooksList';
+import NewBook from './NewBook/NewBook';
 
 const App = () => {
   return (
     <div className="app">
+      <NewBook />
       <BooksList />
     </div>
   );
---
+++ src/NewBook/NewBook.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './NewBook.css';
+
+const NewBook = () => {
+  return <div className="new-book"></div>;
+};
+
+export default NewBook;
